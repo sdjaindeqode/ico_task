c5df7952f7c10473187482e23e95dc25d2644bc3
Siddhant Jain <sdjain@deqode.com>
2021-09-03 14:41:21 +0530

    docs: added documenation

--- README.md
+++ README.md
@@ -25,4 +25,83 @@ To run celery run this commands:
     celery -A ico.celery worker --loglevel=info --beat
 
 
+## Accessing data through API
+### Authentication of user
+For registration of user:
 
+    method: `post`
+    endpoint: `/auth/registration/`
+    args: `email`, `username`, `password1`, `password2`
+    returns: `key` (token that will be used for authentication)
+
+For login of user:
+
+    method: `post`
+    endpoint: `/auth/login/`
+    args: `username`, `password`
+    returns: `key` (token that will be used for authentication)
+    
+For logout of user:
+
+    method: `post`
+    endpoint: `/auth/logout/`
+    headers: `Authorization : Token <your-token>`
+    returns: `detail`
+    
+### Working with other routes
+For accessing token available for biddings:
+    
+    method: `get`
+    endpoint: `/api/v1/token/`
+    returns: `list of tokens available`
+    
+For accessing specific token:
+     
+    method: `get`
+    endpoint: `/api/v1/token/<int:pk>`
+    returns: `token object`
+    
+For creating new bid:
+    
+    method: `post`
+    endpoint: `/api/v1/bid/`
+    headers: `Authorization : Token <your-token>`
+    data: `token`, `number_of_token`, `bid_price`
+    returns: `token object`
+    
+For accessing the bid:
+    
+    method: `get`
+    endpoint: `/api/v1/bid/<int:pk>`
+    headers: `Authorization : Token <your-token>`
+    returns: `token object`
+
+For updating the bid:
+    
+    method: `put`
+    endpoint: `/api/v1/bid/<int:pk>`
+    headers: `Authorization : Token <your-token>`
+    data: `number_of_token`, `bid_price`
+    returns: `token object`
+
+
+For deleting the bid:
+    
+    method: `delete`
+    endpoint: `/api/v1/bid/<int:pk>`
+    headers: `Authorization : Token <your-token>`
+    returns: Empty response
+
+
+For accessing successfull bid:
+    
+    method: `get`
+    endpoint: `/api/v1/bid/success`
+    returns: `list of successfull bids`
+
+
+For accessing failed bid:
+    
+    method: `get`
+    endpoint: `/api/v1/bid/failed`
+    returns: `list of failed bids`
--- ico_bids/views.py
+++ ico_bids/views.py
@@ -15,16 +15,34 @@ from .models import Token, Bid
 # Create your views here.
 
 class TokenView(ListAPIView):
+    """
+    This view is used to request list of token available for offering.
+    Methods: GET
+    Permissions: AllowAny
+    Endpoint: 'api/v1/token/'
+    """
     permission_classes = (AllowAny,)
     serializer_class = TokenSerializer
     queryset = Token.objects.all()
 
 class TokenDetailView(RetrieveAPIView):
+    """
+    This view is used to request a specific token's detail.
+    Methods: GET
+    Permissions: AllowAny
+    Endpoint: 'api/v1/token/<int:pk>'
+    """
     permission_classes = [AllowAny,]
     serializer_class = TokenSerializer
     queryset = Token.objects.all()
 
 class BidDetailView(RetrieveUpdateDestroyAPIView):
+    """
+    This view is used to retreive, update and delete the request bid.
+    Methods: GET, PUT, DELETE
+    Permissions: IsAuthenticated
+    Endpoint: 'api/v1/bid/<int:pk>'
+    """
     permission_classes = [IsAuthenticated,]
     serializer_class = BidSerializer
 
@@ -35,17 +53,49 @@ class BidDetailView(RetrieveUpdateDestroyAPIView):
         )
 
     def update(self, request, *args, **kwargs):
+        """
+        Called by HTTP PUT method used to update the bid.
+        Authorization header should be passed to request.
+        Example - Authorization : Token <user-token obtained while login>
+        Args:
+            number_of_token: number of token needed by user.
+            bid_price: bidding price for the token.
+
+        Returns:
+            Returns the newly updated bid details
+        """
         kwargs['partial'] = True
         request.data['user'] = self.request.user.id
-        request.data.pop('token')
+        if request.data.get('token'):
+            request.data.pop('token')
         return super().update(request, *args, **kwargs)
 
 
 class CreateBidView(CreateAPIView):
+    """
+    This view is used to create a new bid.
+    Methods: POST
+    Permissions: IsAuthenticated
+    Endpoint: 'api/v1/bid/'
+    """
     permission_classes = [IsAuthenticated,]
     serializer_class = BidSerializer
 
     def create(self, request, *args, **kwargs):
+        """
+        Called by HTTP POST method used to create a new bid.
+        Authorization header should be passed to request.
+        Example - Authorization : Token <user-token obtained while login>
+        Args:
+            token: token_id on which user want to bid.
+            number_of_token: number of token needed by user.
+            bid_price: bidding price for the token.
+
+        Returns:
+            Returns the newly created bid details
+            If the user already bid on the same token previously
+            then, that will be returned.
+        """
         data = request.data
         data['user'] = self.request.user.id
         serializer = self.get_serializer(data=data)
@@ -72,12 +122,24 @@ class CreateBidView(CreateAPIView):
 
 
 class SuccessBidView(ListAPIView):
+    """
+    This view is used to return all the successfull bids.
+    Methods: GET
+    Permissions: AllowAny
+    Endpoint: 'api/v1/bid/success'
+    """
     permission_classes = (AllowAny,)
     serializer_class = BidSerializer
     queryset = Bid.objects.filter(status=Bid.STATUS[0][0])
 
 
 class FailedBidView(ListAPIView):
+    """
+    This view is used to return all the failed bids.
+    Methods: GET
+    Permissions: AllowAny
+    Endpoint: 'api/v1/bid/failed'
+    """
     permission_classes = (AllowAny,)
     serializer_class = BidSerializer
     queryset = Bid.objects.filter(status=Bid.STATUS[1][0])
